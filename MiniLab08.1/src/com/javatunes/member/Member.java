036aad6dbd78d3a3c7c6595f8b11864c4133c9dc
Jorge Aponte <jorgealyaponte@yahoo.com>
2023-02-20 15:22:41 -0800

    MiniLab08.1 Complete

--- MiniLab08.1/src/com/javatunes/member/Member.java
+++ MiniLab08.1/src/com/javatunes/member/Member.java
@@ -12,7 +12,8 @@ public class Member {
 
     public Member() {
         // NOTE: there is a call to super() here! as the first line of code
-        // What's the superclass of Member?  Is there a no-arg ctor in that class?
+        // What's the superclass of Member? Class Object.
+        // Is there a no-arg ctor in that class? Yes there is.
         System.out.println("Member ctor");
     }
 }
