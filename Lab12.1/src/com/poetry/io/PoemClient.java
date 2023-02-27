6dac7eb76032d345df75580a045052cea87f5413
Jorge Aponte <jorgealyaponte@yahoo.com>
2023-02-27 09:44:25 -0800

    Lab 12.1 Completed

--- Lab12.1/src/com/poetry/io/PoemClient.java
+++ Lab12.1/src/com/poetry/io/PoemClient.java
@@ -8,8 +8,9 @@
 
 package com.poetry.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class PoemClient {
 
@@ -18,7 +19,7 @@ public class PoemClient {
      */
     public static void main(String[] args) {
         // readPoem();
-        // writePoem();
+        writePoem();
     }
 
     /**
@@ -34,27 +35,56 @@ public class PoemClient {
      * The try-with-resources below allows you to initialize the stream and auto-close it.
      */
     private static void readPoem() {
-        // TODO: initialize 'reader' variable and complete the try block
-        try (BufferedReader reader = null) {
-
-        }
-        catch (IOException e) {
+        try {
+            String poem = Files.readString(Path.of("famous-poem.txt"));
+            System.out.println(poem);
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+        /*
+        try (BufferedReader reader = new BufferedReader(new FileReader("famous-poem.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
     }
 
     /**
      * TASK: write a Haiku to file 'haiku.txt'.
      *
      * A Haiku is a 3-line poem with the following characteristics:
-     *  1st line is 5 syllables
-     *  2nd line is 7 syllables
-     *  3rd line is 5 syllables
+     * 1st line is 5 syllables "How are you doing?
+     * 2nd line is 7 syllables "I am doing great, and you?
+     * 3rd line is 5 syllables ""
      *
      * Use a PrintWriter wrapped around a FileWriter.
      * Use a try-with-resources to initialize the stream and auto-close it.
      */
     private static void writePoem() {
-        // TODO
+        String haiku = "How are you doing \n" +
+                "I am doing great, and you? \n" +
+                "Thanks for asking me.";
+
+        try {
+            Files.writeString(Path.of("haiku.txt"), haiku);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*
+        try (PrintWriter out = new PrintWriter(new BufferedWriter((new FileWriter("haiku.txt"))))) {
+            out.println("How are you doing?");
+            out.println("I am doing great, and you?");
+            out.println("Thanks for asking me.");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
     }
 }
